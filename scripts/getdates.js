fef44cd0277a1871848fc6f5d7fc510c466a4474
Zachary Humphreys <zachary.humphreys32@gmail.com>
2024-04-26 00:13:45 -0400

    Website mockup

---
+++ scripts/getdates.js
@@ -0,0 +1,10 @@
+function updateFooterTime() {
+    let currentDate = new Date()
+    let footerInfoContent = `&copy${currentDate.getFullYear()}</br>Zachary William Humphreys</br>Georgia, USA 🇺🇸`
+    let footerTimeContent = `Last Modified: ${document.lastModified}`
+
+    document.querySelector("#footerInformation").innerHTML = footerInfoContent;
+    document.querySelector("#lastModified").innerHTML = footerTimeContent;
+}
+
+updateFooterTime();
