8992fea6b2e95454e5d2f3676d8e32210fee6d4f
Zachary Humphreys <zachary.humphreys32@gmail.com>
2024-07-14 21:49:37 -0400

    Uploaded current progress on final project.

---
+++ scoots/scripts/populateRentals.js
@@ -0,0 +1,44 @@
+const rentalTable = document.querySelector("#rental-table");
+let url = "data/rentals.json"
+
+async function fetchData(url) {
+    let request = await fetch(url);
+    try {
+        if (request.ok) {
+            let data = await request.json();
+            populateTable(data);
+        }
+    }
+    catch (error) {
+        console.log(error);
+    }
+}
+
+function createRentalCell(content, row) {
+    let rentalCell = document.createElement("td");
+    rentalCell.setAttribute("scope", "row");
+    rentalCell.textContent = content;
+    row.appendChild(rentalCell);
+
+    return rentalCell
+}
+
+function buildTableRow(obj) {
+    let row = document.createElement("tr");
+    let type = createRentalCell(obj.type, row);
+    let max = createRentalCell(obj.max, row);
+    let resHalf = createRentalCell(obj.reservation.halfDay, row);
+    let resFull = createRentalCell(obj.reservation.fullDay, row);
+    let walkHalf = createRentalCell(obj.walkIn.halfDay, row);
+    let walkFull = createRentalCell(obj.walkIn.fullDay, row);
+
+    rentalTable.appendChild(row);
+}
+
+function populateTable(data) {
+    data.rentals.forEach(obj => {
+        buildTableRow(obj)
+    });
+}
+
+fetchData(url);
